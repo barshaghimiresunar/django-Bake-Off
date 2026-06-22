5b134f9049d2182e619a9b2f227d5befc19e998f
barshaghimiresunar <barsha.sunar@kickdrumtech.com>
2026-06-22 14:41:47 +0530

    fix(BAK-32): use \A and \Z anchors in UnicodeUsernameValidator regex

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -17,7 +17,7 @@ class ASCIIUsernameValidator(validators.RegexValidator):
 
 @deconstructible
 class UnicodeUsernameValidator(validators.RegexValidator):
-    regex = r'^[\w.@+-]+$'
+    regex = r'\A[\w.@+-]+\Z'
     message = _(
         'Enter a valid username. This value may contain only letters, '
         'numbers, and @/./+/-/_ characters.'
