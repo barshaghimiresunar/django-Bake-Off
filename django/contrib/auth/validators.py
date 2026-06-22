358263660168f4a270aaf1fbbf03c5933dfd8a80
barshaghimiresunar <barsha.sunar@kickdrumtech.com>
2026-06-22 20:51:48 +0530

    fix(BAK-36): Update regex to reject usernames with trailing newline

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -4,7 +4,7 @@ from django.core import validators
 from django.utils.deconstruct import deconstructible
 from django.utils.translation import gettext_lazy as _
 
-
+regex = r'\\A[\\w.@+-]+\\Z'
 @deconstructible
 class ASCIIUsernameValidator(validators.RegexValidator):
     regex = r'^[\w.@+-]+$'
