a1d26ef57f7677ccd5dcdb73a03bf76698f14253
barshaghimiresunar <barsha.sunar@kickdrumtech.com>
2026-06-22 11:28:55 +0530

    fix(BAK-5): Use \A...\Z anchors in ASCIIUsernameValidator to reject trailing newlines

--- django/contrib/auth/validators.py
+++ django/contrib/auth/validators.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 
 @deconstructible
 class ASCIIUsernameValidator(validators.RegexValidator):
-    regex = r"^[\w.@+-]+\Z"
+    regex = r"\A[\w.@+-]+\Z"
     message = _(
         "Enter a valid username. This value may contain only unaccented lowercase a-z "
         "and uppercase A-Z letters, numbers, and @/./+/-/_ characters."
