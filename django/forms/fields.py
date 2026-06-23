303c3f64e0a96a32eaaf4858c2534124ab45129e
barshaghimiresunar <barsha.sunar@kickdrumtech.com>
2026-06-23 11:49:33 +0530

    fix(BAK-44): add format hint to DurationField invalid error message
    The 'invalid' error message previously gave no format hint. Updated it
    to include the correct format string '[DD] [[HH:]MM:]ss[.uuuuuu]',
    matching the actual behaviour of standard_duration_re where seconds are
    mandatory, minutes are optional, and hours are only valid when minutes
    are also present.
    
    Fixes: https://github.com/barshaghimiresunar/django-Bake-Off/issues/29

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -468,7 +468,7 @@ class DateTimeField(BaseTemporalField):
 
 class DurationField(Field):
     default_error_messages = {
-        'invalid': _('Enter a valid duration.'),
+        'invalid': _('Enter a valid duration. The format is [DD] [[HH:]MM:]ss[.uuuuuu].'),
         'overflow': _('The number of days must be between {min_days} and {max_days}.')
     }
 
